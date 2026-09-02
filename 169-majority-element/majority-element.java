c18adec331b0c408883ab679633ec53f4dfc2e21
SARVESH SHINDE <138493787+sarvesh2846@users.noreply.github.com>
2026-09-02 09:37:25 +0530

    Time: 8 ms (40.41%) | Memory: 55.2 MB (70.17%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,28 @@
+import java.util.Arrays;
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
-        for (int val : nums) {
-            int freq = 0;
-            for (int el : nums) {
-                if (el == val) {
-                    ++freq;
-                }
+        Arrays.sort(nums);
+        int freq = 1, ans = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == nums[i-1]){
+                ++freq;
+            } 
+            else {
+                freq = 1;
+                ans = nums[i]; 
+
             }
-            if (freq > n / 2) {
-                return val;
+            
+            if(freq > n/2){
+                return ans;
             }
         }
-        return -1;
+        return ans;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

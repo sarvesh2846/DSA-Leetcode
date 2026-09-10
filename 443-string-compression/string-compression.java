ff2142bb570d6bd68365d0b473b77f8a20146b95
SARVESH SHINDE <138493787+sarvesh2846@users.noreply.github.com>
2026-09-11 01:11:29 +0530

    Time: 1 ms (99.92%) | Memory: 45.3 MB (65.32%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int compress(char[] chars) {
+        int n = chars.length;
+        int idx = 0;
+
+        for(int i = 0; i < n; i++) {
+            char ch = chars[i];
+            int count = 0;
+
+            //Count the consecutive repeating char
+            while(i < n && chars[i] == ch){
+                count++;
+                i++;
+            }
+
+            //conditions 
+            if(count == 1){
+                chars[idx++] = ch;//chars[idx] = ch; //idx++;
+            }
+            else{
+                chars[idx++] = ch;
+               
+                String str = String.valueOf(count); //int => string
+                for (char chr : str.toCharArray()) { // string => charArray => Each char (['count',])
+                   chars[idx++] = chr;
+                    
+                }
+            }
+            i--;
+        }
+
+        return idx; // New length of Array
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

149b59a6b144f20081f364271fdbc03a75f96111
SARVESH SHINDE <138493787+sarvesh2846@users.noreply.github.com>
2026-09-11 12:59:22 +0530

    Time: 4 ms (83.77%) | Memory: 44.6 MB (59.68%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n1 = s.length(), n2 = t.length();
+
+        if(n1 != n2) return false;
+
+        int[] count = new int[26];
+        for(int i = 0; i < n1; i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for(int i = 0; i < count.length; i++){
+            if(count[i] != 0) return false;
+        }
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

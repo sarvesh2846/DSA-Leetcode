6a67d93ac7436d3f720ad96992d70526ecfc7edc
SARVESH SHINDE <138493787+sarvesh2846@users.noreply.github.com>
2026-09-11 19:18:38 +0530

    Time: 1 ms (99.83%) | Memory: 43.4 MB (85.37%) - LeetSync

---
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+            StringBuilder sb = new StringBuilder(s);
+
+        while(sb.length() > 0 && sb.indexOf(part) != -1){
+               
+            int start = sb.indexOf(part);
+            sb.delete(start, start + part.length());
+            //end index is not included
+
+        }
+        return sb.toString();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

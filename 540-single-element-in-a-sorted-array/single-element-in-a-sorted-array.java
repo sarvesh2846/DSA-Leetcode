dfce53403bc684aff2dffec2b76201cfc03c864f
SARVESH SHINDE <138493787+sarvesh2846@users.noreply.github.com>
2026-09-14 23:27:17 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (9.38%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        
+        //Edge case:
+        if(n == 1) return nums[0];
+
+        int st = 0, end = n-1;
+
+        while(st <= end){
+            int mid = st + (end - st)/2;
+
+            //Corner Cases:
+            if(mid == 0  && nums[0] != nums[1]) return nums[mid];
+            if(mid == n-1 && nums[n-1] != nums[n-2]) return nums[mid];
+
+            // Element finding Condtions 
+            if(nums[mid-1] != nums[mid] && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }
+            
+            //Where we search our Element(in which side: Left or Right)
+            if(mid % 2 == 0){ //even 
+                if(nums[mid-1] == nums[mid]){ //left
+                    end = mid-1;
+                }
+                else{ //right
+                    st = mid+1;
+                }
+            }
+            else{ //odd
+                if(nums[mid-1] == nums[mid]){ //right
+                    st = mid+1;
+                }
+                else{ //left
+                    end = mid-1;
+                }
+
+            }
+        
+        }
+        return -1;
+    }
+}

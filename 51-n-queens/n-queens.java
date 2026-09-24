8635f790258c7c9936b5cb1c31adb1ccaca8f60f
SARVESH SHINDE <138493787+sarvesh2846@users.noreply.github.com>
2026-09-24 11:20:00 +0530

    Time: 11 ms (5.90%) | Memory: 46.7 MB (27.98%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+class Solution {
+
+    public static boolean isSafe(List<String> board, int row, int col, int n){
+        //horizontal
+        for(int j = 0 ;j < n; j++){
+            if(board.get(row).charAt(j) == 'Q'){
+                return false;
+            }
+        }
+        //vertical
+         for(int i = 0 ;i < n; i++){
+            if(board.get(i).charAt(col) == 'Q'){
+                return false;
+            }
+        }
+        // left diagonal
+         for(int i = row, j = col; i >= 0 && j >= 0; i--, j--){
+            if(board.get(i).charAt(j) == 'Q'){
+                return false;
+            }
+        }
+        // Right diagonal
+        for(int i = row, j = col; i >= 0 && j < n; i--, j++){
+            if(board.get(i).charAt(j) == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void nQueens(List<String> board, int row, int n, List<List<String>> ans){
+        if(row == n){
+            ans.add(new ArrayList<>(board)); // we get our one the sol then immediatly add into ans string 
+            return;
+        }
+
+        for(int j = 0; j < n; j++){
+            if(isSafe(board, row, j, n)){
+                
+                // Place Queen
+                StringBuilder str = new StringBuilder(board.get(row));
+                str.setCharAt(j, 'Q');
+                board.set(row, str.toString());
+
+                // Recursive call
+                nQueens(board, row + 1, n, ans);
+
+                // Backtracking
+                str.setCharAt(j, '.');
+                board.set(row, str.toString());
+            }
+        }
+    }
+
+    public List<List<String>> solveNQueens(int n) {
+        List<String> board = new ArrayList<>();
+
+        // Create n rows of "...."
+        String emptyRow = ".".repeat(n);
+
+        // board => "...." , "....", "....", "...."  
+        for(int i = 0; i < n; i++){
+            board.add(emptyRow);
+        }
+
+        List<List<String>> ans = new ArrayList<>(); // initiase the ans List
+
+        nQueens(board, 0, n, ans);
+
+        return ans;
+    }
+}

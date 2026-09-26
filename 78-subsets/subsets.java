707c5f3457cf06c2f24de997785b67248bc3fc78
SARVESH SHINDE <138493787+sarvesh2846@users.noreply.github.com>
2026-09-26 10:43:49 +0530

    Time: 1 ms (87.10%) | Memory: 44.3 MB (39.07%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
-class Solution {
+// O(n * 2^n)
+class Solution { 
 
     public static void getAllSubsets(int[] nums, List<Integer> ans, int i, List<List<Integer>> allSubsets){
         

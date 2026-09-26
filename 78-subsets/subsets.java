c94afbbfbd91af50ed328a4d5e2b518a190f391c
SARVESH SHINDE <138493787+sarvesh2846@users.noreply.github.com>
2026-09-26 10:35:41 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (7.26%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+
+    public static void getAllSubsets(int[] nums, List<Integer> ans, int i, List<List<Integer>> allSubsets){
+        
+        if(i == nums.length){
+            allSubsets.add(new ArrayList<>(ans));
+            return;
+        }
+        
+        //include
+        ans.add(nums[i]);
+        getAllSubsets(nums, ans, i+1, allSubsets);
+
+        //remove last element from list    
+        ans.remove(ans.size() - 1); // bactrack
+
+        //exclude
+        getAllSubsets(nums, ans, i+1, allSubsets);
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> allSubsets = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+
+        getAllSubsets(nums, ans, 0, allSubsets);
+        return allSubsets;
+    }
+}
